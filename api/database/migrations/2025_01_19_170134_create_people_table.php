9e01ca76d6b8cf412f93283132fd76da7ff97bfe
Rafael Gil <rafaelllcomputadores892@gmail.com>
2025-01-19 14:05:54 -0300

    create model and migration

---
+++ api/database/migrations/2025_01_19_170134_create_people_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('nickname');
+            $table->string('name');
+            $table->string('birth');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('people');
+    }
+};
