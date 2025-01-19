211cfacca543035efe2da944f3c4b4ad50c8a4d8
Rafael Gil <rafaelllcomputadores892@gmail.com>
2025-01-19 16:28:45 -0300

    Added CreateComponent in person components

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './person/home/home.component';
 import { ModalComponent } from './modal/modal.component';
 import { EditComponent } from './person/edit/edit.component';
+import {CreateComponent} from "./person/create/create.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ModalComponent,
+    CreateComponent,
     EditComponent
   ],
   imports: [
